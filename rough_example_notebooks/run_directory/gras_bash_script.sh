d190ea452105c14e3560c2c2926ee13d5548aaa8
Chris S. W. Davis <ChrisSWDavis@gmail.com>
2026-03-10 16:26:26 +0000

    Adding more rough example notebook data

---
+++ rough_example_notebooks/run_directory/gras_bash_script.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /home/chrisswdavis/Geant4Installations/geant4-v10.7.4/install/bin/geant4.sh
+source /home/chrisswdavis/gras-06-00-beta/config/gras-env.sh
+gras gras_input_macro.in
